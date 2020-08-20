b4c5aaa8abf2ead52db60b7d0675b6a91d0cf99d
manu <manu.villar.vitoria@gmail.com>
2020-08-20 22:27:20 +0200

    base project

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Inicio</h1>
+
+      <style jsx>{`
+        h1 {
+          color: red;
+        }
+      `}</style>
+      
+    </div>
+  )
+}
+
+export default Home
