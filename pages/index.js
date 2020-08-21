aa983971d78b660a97121bfd59ccb6a9afd4cd10
manu <manu.villar.vitoria@gmail.com>
2020-08-21 14:40:27 +0200

    Adding normalize CDN, Google Fonts, custom css file & images

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
-import styled from '@emotion/styled';
 
-const Heading = styled.h1 `
-  color: red;
-`;
 
 const Home = () => {
   return (
     <div>
       <Layout>
-        <Heading>Inicio</Heading>
+        <h1>Inicio</h1>
       </Layout>
     </div>
   )
