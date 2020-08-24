84d0b139f2c717b3be50691d3f2d6e1b23d2e218
manu <manu.villar.vitoria@gmail.com>
2020-08-24 17:13:30 +0200

    creating a function to validate forms & importing Custom Hook to crear-cuenta.js page

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -5,57 +5,76 @@ import { css, jsx } from '@emotion/core';
 import Layout from '../components/layout/Layout';
 import { Form, Field, InputSubmit } from '../public/static/styles/Form';
 
+import useValidacion from '../hooks/useValidacion';
+import validarCrearCuenta from '../validacion/validarCrearCuenta';
+
+
+const STATE_INICIAL = {
+    nombre: '',
+    email: '',
+    password: ''
+}
+
 
 const CrearCuenta= () => {
-  return (
-    <div>
-      <Layout>
-        <>
-            <h1
-                css={css `
-                    text-align: center;
-                    margin-top: 5rem;
-                `}
-            >Crear Cuenta</h1>
-            <Form>
-                <Field>
-                    <label htmlFor="Nombre">Nombre</label>
-                    <input 
-                        type="text"
-                        id="nombre"
-                        placeholder="Tu Nombre"
-                        name="nombre"
-                    />
-                </Field>
 
-                <Field>
-                    <label htmlFor="email">Email</label>
-                    <input 
-                        type="email"
-                        id="email"
-                        placeholder="Tu Email"
-                        name="email"
-                    />
-                </Field>
+    const { valores, errores, submitForm, handleSubmit, handleChange } = useValidacion
+    (STATE_INICIAL, validarCrearCuenta, CrearCuenta);
 
-                <Field>
-                    <label htmlFor="password">Pasword</label>
-                    <input 
-                        type="password"
-                        id="password"
-                        placeholder="Tu Password"
-                        name="password"
-                    />
-                </Field>
+    function CrearCuenta() {
+        console.log('Crear Cuenta...');
+    }
 
-                <InputSubmit 
-                    type="submit"
-                    value="Crear Cuenta"
-                />
-            </Form>
-        </>
-      </Layout>
-    </div>
+
+    return (
+        <div>
+            <Layout>
+                <>
+                    <h1
+                        css={css `
+                            text-align: center;
+                            margin-top: 5rem;
+                        `}
+                    >Crear Cuenta</h1>
+                    <Form>
+                        <Field>
+                            <label htmlFor="Nombre">Nombre</label>
+                            <input 
+                                type="text"
+                                id="nombre"
+                                placeholder="Tu Nombre"
+                                name="nombre"
+                            />
+                        </Field>
+
+                        <Field>
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                type="email"
+                                id="email"
+                                placeholder="Tu Email"
+                                name="email"
+                            />
+                        </Field>
+
+                        <Field>
+                            <label htmlFor="password">Pasword</label>
+                            <input 
+                                type="password"
+                                id="password"
+                                placeholder="Tu Password"
+                                name="password"
+                            />
+                        </Field>
+
+                        <InputSubmit 
+                            type="submit"
+                            value="Crear Cuenta"
+                        />
+                    </Form>
+                </>
+            </Layout>
+        </div>
   )
 }
 
