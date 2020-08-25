66effc691e7365338410fdbc954477774c9e5806
manu <manu.villar.vitoria@gmail.com>
2020-08-25 18:32:46 +0200

    saving the authenticated user even when to reload using a Custom Hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import App from 'next/app';
 import firebase, { FirebaseContext } from '../firebase'
+import useAutenticacion from '../hooks/useAutenticacion';
+
 
 const  MyApp = props => {
 
+  const usuario = useAutenticacion();
+ 
   const { Component, pageProps } = props;
 
   return (
