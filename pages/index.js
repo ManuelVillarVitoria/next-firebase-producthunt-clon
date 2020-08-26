50f76aea7b6874087de3a4d4f3b28ea057e6f6a5
manu <manu.villar.vitoria@gmail.com>
2020-08-26 12:55:56 +0200

    making a query to the DB to display the registers

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
 
 
 const Home = () => {
+
+  const [ productos, guardarProductos ] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot
+      (manejarSnapshot)
+    }
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+    guardarProductos(productos)
+  }
+
   return (
     <div>
       <Layout>
