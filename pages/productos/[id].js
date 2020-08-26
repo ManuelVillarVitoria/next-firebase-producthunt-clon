9e3427eb41f5a9edab49d21b204a85056f30fe44
manu <manu.villar.vitoria@gmail.com>
2020-08-26 20:13:58 +0200

    adding the 'Markup' for the  the products

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useRouter } from 'next/router';
 
+/**@jsx jsx */
+import { css, jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+
 import { FirebaseContext } from '../../firebase';
+
 import Layout from '../../components/layout/Layout';
 import Error404 from '../../components/layout/404';
+
+
+const ContenedorProducto = styled.div `
+    @media (min-width:768px) {
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        column-gap: 2rem;
+    }
+`;
 
 
 const Producto = () => {
@@ -31,11 +45,33 @@ const Producto = () => {
         }
     }, [id]);
 
+    if(Object.keys(producto).length === 0) return 'Cargando...';
+
+    const {  comentarios, creado, descripcion, empresa, nombre, url, urlImagen,votos } 
+    = producto;
+
 
     return (
         <Layout>
             <>
                 { error && <Error404 />}
+
+                <div>
+                    <h1 css={css `
+                            text-align: center;
+                            margin-top: 5rem;
+                    `}>{nombre}</h1>
+
+                    <ContenedorProducto>
+                        <div>
+                        1
+                        </div>
+
+                        <aside>
+                        2
+                        </aside>
+                    </ContenedorProducto>
+                </div>
             </>
         </Layout>
     )
